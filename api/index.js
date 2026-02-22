1105927c13a59a815f76020510a537c8f3421bf0
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-02-22 15:17:41 -0500

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,12 @@
+{
+  "name": "Daily Reading Audio API",
+  "description": "A free REST API for USCCB Daily Mass Reading Audio.",
+  "version": "1.0.0",
+  "endpoints": {
+    "today": "/api/today",
+    "yesterday": "/api/yesterday",
+    "tomorrow": "/api/tomorrow",
+    "month": "/api/month"
+  },
+  "author": "Eric Kouassi"
+}
